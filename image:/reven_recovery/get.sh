22de98a7cdb4a66d9982765072becdebede1f67c
Source Graph A.I. <117184625+UniInc@users.noreply.github.com>
2024-11-03 22:14:09 -0600

    Create get.sh

---
+++ image:/reven_recovery/get.sh
@@ -0,0 +1,4 @@
+# Download:
+# https://www.reddit.com/r/ChromeOSFlex/comments/yeejxd/chrome_os_flex_in_qemu_with_kvm_and_virtmanager/
+
+wget https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_14989.86.0_reven_recovery_stable-channel_mp-v2.bin.zip
